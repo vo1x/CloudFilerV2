36c567df552300f83974e578429e91f7c7049732
volx <97158976+vo1x@users.noreply.github.com>
2024-04-12 12:24:06 +0545

    Add kamchalau navbar

--- src/components/FormHeader.jsx
+++ src/components/FormHeader.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
 function Header() {
-  return <div className="flex flex-col mb-4 text-2xl font-bold">Cloudfiler </div>;
+  return (
+    <div className="flex w-full max-w-96 items-center justify-between p-3">
+      <div className="text-2xl font-bold">Cloudfiler</div>
+      <div className="flex items-center gap-3">
+        <Link to={`/indexer`}>
+          <button className="">Indexer</button>
+        </Link>
+
+        <Link to={`/builder`}>
+          <button className="">Post Builder</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
 function Header() {
-  return <div className="p-5 text-4xl font-bold">Cloudfiler</div>;
+  return (
+    <div className="flex max-w-96 items-center w-full justify-between p-3">
+      <div className='font-bold text-2xl'>Cloudfiler</div>
+      <div className="flex gap-3 items-center">
+        <Link to={`/indexer`}>
+          <button className="">Indexer</button>
+        </Link>
+
+        <Link to={`/builder`}>
+          <button className="">Post Builder</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
