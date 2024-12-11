da0fc786aa586390c5e37d6ea97c42bda0dbd275
volx <97158976+vo1x@users.noreply.github.com>
2024-12-11 19:36:05 +0545

    Remove unused files and imports

--- src/components/FileTable/FileTable.jsx
+++ src/components/FileTable/FileTable.jsx
@@ -1,30 +1,8 @@
-import useFileSize from '../../hooks/useFileSize';
-import { MdMovie, MdFolder } from 'react-icons/md';
-import useContentStrings from '../../hooks/useContentStrings';
-import useMovieStrings from '../../hooks/useMovieString';
-import Button from './Button';
-import useClipboard from '../../hooks/useClipboard';
-import Row from './Row';
-
-import { SearchIcon, Link2 } from 'lucide-react';
 import { memo } from 'react';
 
+import Row from './Row';
+
 const FileTable = memo(({ extractResults }) => {
-  console.log(extractResults);
-  const { getReadableFS } = useFileSize();
-
-  const handleFindButtonClick = (fileName) => {
-    const regExp = /^(.*?)\s[Ss]\d{2}(?:[Ee]\d{2})?/;
-
-    const match = fileName.match(regExp);
-    console.log(fileName);
-    if (match[1]) {
-      return window.open(`https://uhdmovies.icu/search/${match[1]}`, '_blank');
-    }
-  };
-
-  const [copied, handleItemCopy] = useClipboard();
-
   return (
     <div className="h-full overflow-auto pr-6">
       <table className="w-full table-auto">
